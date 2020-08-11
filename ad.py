fe5b83278022b342d64908a82e872a05aec8143a
Shreya Bhattacharya <64976753+theshreyathing@users.noreply.github.com>
2020-08-11 11:06:24 +0530

    Update ad.py

--- ad.py
+++ ad.py
@@ -1,9 +1,9 @@
 
 answer = input("Would you like to play? (yes/no)")
 if answer.lower().strip() == "yes":
-    input("Welcome to the Game, You come accross a two way street. Which way do you want to go (left/right)")
+    answer = input("Welcome to the Game, You come accross a two way street. Which way do you want to go (left/right)")
     if answer == "left" :
-        input("You were walking and encountered a monster. Do you want to attack or run?(attack/run)")
+        answer = input("You were walking and encountered a monster. Do you want to attack or run?(attack/run)")
         if  answer == "attack" :
              print("aaww!! You're too week to fight. GAME OVER")
         elif answer == "run":
@@ -11,7 +11,7 @@ if answer.lower().strip() == "yes":
         else:
             print(" INCORRECT VALUE : GAME OVER")
     elif answer == "right":
-        input("You are in a garden. You saw fruits. Do you want to eat or let go (eat/let go)")
+        answer = input("You are in a garden. You saw fruits. Do you want to eat or let go (eat/let go)")
         if answer == "eat":
             print("Health is good. On to next level. MISSION ACCOMPLISED : WINNER. Wait for next level")
         else:              
